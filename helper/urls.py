b189107f1744a602311050d58018b2edd6fb65b2
Deji <thedejijoseph@gmail.com>
2025-07-30 16:18:30 +0100

    add endpoint to get subscriber count

--- helper/urls.py
+++ helper/urls.py
@@ -1,9 +1,11 @@
 # helper/urls.py
 from django.urls import path
-from .views import SubmitEmailView, VerifyTelegramView, VerifyDiscordView, VerifyJoindaAccountView
+from .views import SubmitEmailView, VerifyTelegramView, VerifyDiscordView, \
+    VerifyJoindaAccountView, SubscriberCountView
 
 urlpatterns = [
     path('submit-email/', SubmitEmailView.as_view(), name='submit_email'),
+    path('subscriber-count/', SubscriberCountView.as_view(), name='subscriber_count'),
     path('verify-telegram/', VerifyTelegramView.as_view(), name='verify_telegram'),
     path('verify-discord/', VerifyDiscordView.as_view(), name='verify_discord'),
     path('verify-signup', VerifyJoindaAccountView.as_view(), name='verify_joinda_signup')
